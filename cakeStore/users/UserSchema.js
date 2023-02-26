a8b9ca8e53887a40f563945424489cab10a888db
Pankaj Mandal <pankaj.mandal@relexhealth.com>
2023-02-26 23:59:46 +0530

    bugs fixed

--- cakeStore/users/UserSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  name: String,
-  phone: Number,
-  email: String,
-  password: String,
-});
-
-module.exports = mongoose.model("users", userSchema);
--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// const cors = require("cors");
-// const express = require("express");
-// const app = express();
-// require("./config");
-// const Products = require("./cakeStore/product");
-// app.use(express.json());
-// app.use(cors());
-// app.get("/list", async (req, resp) => {
-//   let data = await Products.find();
-//   resp.send(data);
-//   console.log(data);
-// });
-// app.post("/create", async (req, resp) => {
-//   let data = new Products(req.body);
-//   let result = await data.save();
-//   console.log(result);
-//   resp.send(result);
-// });
-// app.listen(5000);
-// // const dataControl = (requ, resp) => {
-// //   resp.write("somethimg here drgg dsgasfg");
-// //   resp.end();
-// // };
-// // http.createServer(dataControl).listen(4000);
-
 const express = require("express");
 const port = 3000;
 
